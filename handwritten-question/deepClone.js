93f81a018b41079cb3bcd7397de9332fc3fdc93a
Yolo-hana63 <553875647@qq.com>
2025-03-24 17:05:14 +0800

    feat: 手写深拷贝

---
+++ handwritten-question/deepClone.js
@@ -0,0 +1,38 @@
+function deepClone(obj, map = new WeakMap()) {
+  if (typeof obj !== "object" || obj === null) {
+    return obj;
+  }
+
+  // 处理循环引用
+  if (map.has(obj)) {
+    return map.get(obj);
+  }
+
+  const newObject = Array.isArray(obj) ? [] : {};
+
+  map.set(obj, newObject);
+
+  for (const key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      if (typeof obj[key] === "object" && obj[key] !== null) {
+        newObject[key] = deepClone(obj[key], map);
+      } else {
+        newObject[key] = obj[key];
+      }
+    }
+  }
+
+  return newObject;
+}
+
+const res = deepClone([1, 2, [3, [4, 5, [6]]]]);
+console.log(res);
+
+console.log(JSON.stringify(res, null, 2));
+
+// 输出结果是[ 1, 2, [ 3, [ 4, 5, [Array] ] ] ]
+// 展示Array简洁展示深度嵌套的结构
+// Json.stringfy的三个参数：
+//  res 是要被转换的对象。
+// null 表示不做任何替换或过滤。
+// 2 表示输出的 JSON 字符串会使用每层两个空格的缩进格式
